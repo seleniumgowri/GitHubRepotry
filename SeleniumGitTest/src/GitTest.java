f16fd74ab0a54776c4e2c5ffb5aef191d629736c
INTEL <INTEL@INTEL-PC>
2020-04-29 19:35:12 +0530

    some features are removed

--- SeleniumGitTest/src/GitTest.java
+++ SeleniumGitTest/src/GitTest.java
@@ -3,10 +3,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		
-		
 		//add some code and come features are added
-		 
-		//some methods are added
+		 //some methods are added
 		
 	}
 
